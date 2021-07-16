7540f6480fdcae0f3e33bdb1fa6ce87beb35e73f
Yuvatej <42350361+Yuvatej@users.noreply.github.com>
2021-07-16 10:06:48 +0530

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -3,6 +3,7 @@ import argparse
 import time
 import cv2
 import os
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,help="path to input image")
@@ -15,6 +16,7 @@ args = vars(ap.parse_args())
 # load the COCO class labels our YOLO model was trained on
 labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
+
 # initialize a list of colors to represent each possible class label
 np.random.seed(41)
 COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),dtype="uint8")
